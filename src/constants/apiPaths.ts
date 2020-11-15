e69816cc4333ca09ec1d67bd30163f70df8ac1f7
Tatsiana Stsepchanka <tstsepchanka@slb.com>
2020-11-15 09:26:27 +0300

    feat: integrate with import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://rsipotklc8.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
