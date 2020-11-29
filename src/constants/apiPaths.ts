a711efa133cb13a7315b5d39e339a9df63b40475
Herman_Bilous <Herman_Bilous@epam.com>
2020-11-29 23:00:38 +0200

    Add cart api integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,7 @@ const API_PATHS = {
   order: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://rsipotklc8.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://zvpmfqknbe.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
